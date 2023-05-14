5b4337bcaa46502e429ac7e9cfe31bd4dc98a6b5
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-05-14 23:46:03 +0530

    implement remaining functions

--- torsposer/exposer.py
+++ torsposer/exposer.py
@@ -39,37 +39,137 @@ class TorServiceExposer:
             raise SudoPermsAbsentException("Run again after running `$ sudo whoami` command in terminal to get sudo perms or run with root user")
 
 
-    def create_torrc_file(self):
+    def install_requirements(self):
+        '''Install required debian packages
+
+        Arguments:
+            None
+
+        Returns:
+            str: returns executed installation output
+        '''
+        # check if tor exists
+        try:
+            output, _ = run_cmd('tor --version')
+            tor_version:str = output.splitlines()[0].split(' ')[2]
+            output = f'TOR found with version {tor_version}'
+            logger.info(output)
+            output
+        except FileNotFoundError:
+            logger.error('TOR is not installed')
+
+            logger.info('Installing TOR package')
+            output, code = run_cmd('sudo apt install tor -y')
+            if code == 0:
+                logger.info('Tor package installed successfully')
+            else:
+                logger.error('Tor package installation failed')
+
+        return output
+
+
+    def create_torrc_file_content(self):
         '''Creates torrc file according to specified configuration in constructor
         
         Arguments:
             None
 
         Returns:
-            boolean: True if successfull else False
+            str: returns tor file content according to specified configuration
         '''
         # read torrc file
         torrc_content = read_file(self.__torrc_path)
         
         # take backup of torrc file
-        bak_file = path_join(getcwd(), f'{self._srv_name}-torrc.bak')
+        bak_file = path_join(getcwd(), f'before-{self._srv_name}-torrc.bak')
         write_file(bak_file, torrc_content)
         logger.info(f'succesfully backed up torrc file at {bak_file}')
 
-        # create torrc configuration
-        torrc_file_content = dedent(f'''
-        ## Enable TOR SOCKS proxy
-        SOCKSPort 127.0.0.1:9050
+        # check if tor file is default tor conf then replace complete conf
+        torrc_content = torrc_content.splitlines()
 
-        ## {self._srv_name} Hidden Service
-        HiddenServiceDir /var/lib/tor/{self._srv_name}
-        HiddenServicePort {self._tor_port} 127.0.0.1:{self._srv_port}
-        ''').strip()
-        
+        if torrc_content[0] == '## Configuration file for a typical Tor user':
+            torrc_file_content = dedent(f'''
+            ## Enable TOR SOCKS proxy
+            SOCKSPort 127.0.0.1:9050
+
+            ## {self._srv_name} Hidden Service
+            HiddenServiceDir /var/lib/tor/{self._srv_name}
+            HiddenServicePort {self._tor_port} 127.0.0.1:{self._srv_port}
+            ''').strip()
+        else:
+            # BUG: can add same service twice
+            # TODO: implement mechanism to check if service is already present 
+            # and avoid adding that service
+
+            torrc_content.append(dedent(f'''
+            ## {self._srv_name} Hidden Service
+            HiddenServiceDir /var/lib/tor/{self._srv_name}
+            HiddenServicePort {self._tor_port} 127.0.0.1:{self._srv_port}\n
+            ''').strip())
+
+            torrc_file_content = '\n'.join(torrc_content)
+
+
         return torrc_file_content
+    
+
+    def write_torrc_file(self, torrc_conf:str) -> bool:
+        '''Writes data to default torrc file
+        
+        Arguments:
+            torrc_conf (str): torrc configuration data
+
+        Returns:
+            bool: True is process is successful else False
+        '''
+        new_torrc_path = path_join(getcwd(), f'{self._srv_name}-torrc')
+        write_file(new_torrc_path, torrc_conf)
+        logger.info(f'Stored New torrc configuration stored at {new_torrc_path}')
+        
+        output, code = run_cmd(f'sudo cp {new_torrc_path} {self.__torrc_path}')
+        logger.debug(f'Output while replacing torrc file:\n{output}')
+
+        return True if code == 0 else False 
+    
+
+    def restart_tor_service(self):
+        '''Restarts tor service
+        
+        Arguments:
+            None
+
+        Returns:
+            None
+        '''
+        _, code = run_cmd('sudo service tor restart')
+        if code == 0:
+            logger.info('TOR service restarted successfully')
+        else:
+            logger.error('Failed to restart TOR service')
 
 
-if __name__ == '__main__':
-    res = TorServiceExposer().create_torrc_file()
+    def run(self):
+        '''starts service automatic configuration, 
+        returns True if service is configured 
+        successfully else returns False
+        
+        Arguments:
+            None
 
-    print(res)
+        Returns:
+            bool: returns True if service is configured successfully else returns False
+        '''
+        self.install_requirements()
+
+        torrc_conf = self.create_torrc_file_content()
+        if self.write_torrc_file(torrc_conf):
+            logger.info('torrc updated successfully')
+            status = True
+        else:
+            logger.error('torrc updation failed. Check debug logs for more info')
+            status = False
+
+        self.restart_tor_service()
+
+        return status
